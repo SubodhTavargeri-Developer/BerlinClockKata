0cba8c07995b6744ddd5102c77dea887f520badb
2022-DEV2-013 <2022dev2013@gmail.com>
2022-11-21 11:42:44 +0530

    add function setClockValues to update seconds value and add testcase to verify if false is returned when seconds value is odd

--- BerlinClockKata/BerlinClock/Clock.swift
+++ BerlinClockKata/BerlinClock/Clock.swift
@@ -1,7 +1,11 @@
 
 class Clock {
     
-    private var seconds: Int = 2
+    private var seconds: Int = 0
+    
+    func setClockValues(seconds: Int) {
+        self.seconds = seconds
+    }
     
     func isSecondLightIlluminated()-> Bool {
         return seconds%2 == 0
--- BerlinClockKataTests/ClockTests.swift
+++ BerlinClockKataTests/ClockTests.swift
@@ -6,8 +6,19 @@ class ClockTests: XCTestCase {
     func test_SecondsLightRowIsIlluminated_WhenSecondsValueIsEven() {
         let clock = Clock()
         
+        clock.setClockValues(seconds: 2)
+        
         let IsIlluminated = clock.isSecondLightIlluminated()
         XCTAssertTrue(IsIlluminated)
+    }
+    
+    func test_SecondsLightRowIsNotIlluminated_WhenSecondsValueIsOdd() {
+        let clock = Clock()
+        
+        clock.setClockValues(seconds: 5)
+        
+        let IsIlluminated = clock.isSecondLightIlluminated()
+        XCTAssertFalse(IsIlluminated)
     }
     
 }
