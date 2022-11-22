53a2b39745f3c31084d3af89b0841392b3541ad7
2022-DEV2-013 <2022dev2013@gmail.com>
2022-11-22 19:33:22 +0530

    refactor Presenter test with setup

--- BerlinClockKataTests/BerlinClockPresenterTests.swift
+++ BerlinClockKataTests/BerlinClockPresenterTests.swift
@@ -2,25 +2,28 @@ import XCTest
 @testable import BerlinClockKata
 
 class BerlinClockPresenterTests: XCTestCase {
+    private var view: BerlinClockViewControllerSpy!
+    private var clock: ClockMock!
+    private var presenter: BerlinClockPresenter!
+    
+    override func setUp() {
+        view = BerlinClockViewControllerSpy()
+        clock = ClockMock()
+        presenter = BerlinClockPresenter(view: view,
+                                         clock: clock)
+    }
     
     func test_ViewTitleIsDisplayed_WhenPresenterLoads() {
-        let view = BerlinClockViewControllerSpy()
-        let clock = ClockMock()
-        let presenter = BerlinClockPresenter(view: view, clock: clock)
-        
         presenter.loadPresenter()
         
         XCTAssertEqual(view.title, Constant.Title.screenTitle)
     }
     
     func test_ComputedFiveHourLightBerlinClockTimeIsEqualToMock_WhenPresenterSendsDigitalTimeToClock() {
-        let view = BerlinClockViewControllerSpy()
-        let clock = ClockMock()
         let berlinClockTime =  BerlinClockTimeBuilder()
             .withFiveHourLights([.Red, .Red, .Off, .Off])
             .build()
         clock.mockBerlinClockTime(berlinClockTime: berlinClockTime)
-        let presenter = BerlinClockPresenter(view: view, clock: clock)
         let digitalClock = DigitalTime(hours: 2, minutes: 2, seconds: 2)
         
         presenter.displayBerlinClock(digitalTime: digitalClock)
