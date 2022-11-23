eebe93cf1e0e7cf4fbdd4d3646f45637545d1b28
2022-DEV2-013 <2022dev2013@gmail.com>
2022-11-23 21:57:37 +0530

    refactor UI testcases

--- BerlinClockKataUITests/BerlinClockKataUITests.swift
+++ BerlinClockKataUITests/BerlinClockKataUITests.swift
@@ -1,12 +1,13 @@
 import XCTest
 
 class BerlinClockKataUITests: XCTestCase {
+    let app = XCUIApplication()
     
-    func test_WhenDigitalTimeIsSixSevenSeventeen_SixHourSevenMinuteSeventeenSecIsSelectedFromPicker() {
-        
-        let app = XCUIApplication()
+    override func setUp() {
         XCUIApplication().activate()
-        
+    }
+    
+    func test_WhenDigitalTimeIsSixHoursSevenMinutesSeventeenSeconds_SixHourSevenMinuteSeventeenSecIsSelectedFromPicker() {
         let textField = app.textFields["digitalTime_textField"]
         textField.tap()
         
@@ -14,21 +15,20 @@ class BerlinClockKataUITests: XCTestCase {
         app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "7 Minute")
         app.pickerWheels.element(boundBy: 2).adjust(toPickerWheelValue: "17 Second")
         app.toolbars["Toolbar"].buttons["Done"].tap()
+        
         XCTAssertEqual(textField.value as? String ?? "", "6:7:17")
     }
     
     func test_SecondLightRowAndHourLightRowExists_WhenTimeSelectedIsSixHourSevenMinuteSeventeenSecIsSelectedFromPicker() {
-        let app = XCUIApplication()
-        XCUIApplication().activate()
-        
         let textField = app.textFields["digitalTime_textField"]
         textField.tap()
         
         app.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "6 Hour")
         app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "7 Minute")
         app.pickerWheels.element(boundBy: 2).adjust(toPickerWheelValue: "17 Second")
+        
         app.toolbars["Toolbar"].buttons["Done"].tap()
-        XCTAssertEqual(textField.value as? String ?? "", "6:7:17")
+  
         let secondValue = app.otherElements["secondsLight0"]
         XCTAssert(secondValue.exists)
         let hourValue = app.otherElements["fiveHourLight0"]
