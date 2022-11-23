1e37f6e4f9c6c32c9b34faa7a906a92cf23027be
2022-DEV2-013 <2022dev2013@gmail.com>
2022-11-23 11:45:24 +0530

    add new custom Time picker with Hour, Minute and Second

--- BerlinClockKata/BerlinClock/View/BerlinViewController.swift
+++ BerlinClockKata/BerlinClock/View/BerlinViewController.swift
@@ -1,18 +1,28 @@
 import UIKit
 
 class BerlinViewController: UIViewController {
-
+    
     private var presenter: BerlinClockPresenter?
+    
+    @IBOutlet private weak var toolBarDone: UIToolbar!
+    @IBOutlet private weak var textFieldTime: UITextField!
+    @IBOutlet private weak var pickerTime: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.loadPresenter()
+        setupView()
     }
     
     func setupPresenter(presenter: BerlinClockPresenter) {
         self.presenter = presenter
     }
-
+    
+    private func setupView() {
+        textFieldTime.inputView = pickerTime
+        textFieldTime.inputAccessoryView = toolBarDone
+    }
+    
 }
 
 extension BerlinViewController: BerlinClockViewProtocol {
@@ -25,4 +35,39 @@ extension BerlinViewController: BerlinClockViewProtocol {
         
     }
     
+}
+
+extension BerlinViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 24
+        case 1,2:
+            return 60
+            
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width/3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row) Hour"
+        case 1:
+            return "\(row) Minute"
+        case 2:
+            return "\(row) Second"
+        default:
+            return ""
+        }
+    }
 }
