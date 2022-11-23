1e9a4a647db93be1243182520a6ef724590c594e
2022-DEV2-013 <2022dev2013@gmail.com>
2022-11-23 09:44:54 +0530

    add new BerlinViewController and update storyboard

--- BerlinClockKata/BerlinClock/View/BerlinViewController.swift
+++ BerlinClockKata/BerlinClock/View/BerlinViewController.swift
@@ -1,12 +1,11 @@
 import UIKit
 
-class ViewController: UIViewController {
+class BerlinViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
