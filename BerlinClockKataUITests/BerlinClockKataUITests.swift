dec786c41e4d7a91f83d862684397fd174527ec8
2022-DEV2-013 <2022dev2013@gmail.com>
2022-11-24 11:50:32 +0530

    update UI testcases for view

--- BerlinClockKataUITests/BerlinClockKataUITests.swift
+++ BerlinClockKataUITests/BerlinClockKataUITests.swift
@@ -19,13 +19,13 @@ class BerlinClockKataUITests: XCTestCase {
         XCTAssertEqual(textField.value as? String ?? "", "6:7:17")
     }
     
-    func test_SecondLightRowAndHourLightRowExists_WhenTimeSelectedIsSixHourSevenMinuteSeventeenSecIsSelectedFromPicker() {
+    func test_SecondLightRowAndHourLightRowExists_WhenTimeSelectedIsEightHourSeventeenMinuteThirtySevenSecIsSelectedFromPicker() {
         let textField = app.textFields["digitalTime_textField"]
         textField.tap()
         
-        app.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "6 Hour")
-        app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "7 Minute")
-        app.pickerWheels.element(boundBy: 2).adjust(toPickerWheelValue: "17 Second")
+        app.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "8 Hour")
+        app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "17 Minute")
+        app.pickerWheels.element(boundBy: 2).adjust(toPickerWheelValue: "37 Second")
         
         app.toolbars["Toolbar"].buttons["Done"].tap()
   
